c46a915226c57e2f120f57acc83ee037b495bf9e
Ignacio Gandur <gandurjuanignacio@gmail.com>
2024-09-29 01:57:50 -0300

    Modified env variables for production

--- db/createTables.js
+++ db/createTables.js
@@ -5,33 +5,45 @@ const { Client } = pg;
 const SQL = `
 
     CREATE TABLE IF NOT EXISTS publishers (
-        id INTEGER GENERATED ALWAYS AS IDENTITY,
-        name VARCHAR(255) UNIQUE,
-        PRIMARY KEY (id)
-    );
-    
-    CREATE TABLE IF NOT EXISTS genres (
-        id INTEGER GENERATED ALWAYS AS IDENTITY,
-        name VARCHAR(255) UNIQUE,
-        PRIMARY KEY (id)
-    );
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    PRIMARY KEY(id),
+    CONSTRAINT unique_publisher_name UNIQUE (name));
 
-    CREATE TABLE IF NOT EXISTS games_covers (
-        id GENERATED ALWAYS AS IDENTITY,
-        game_id INTEGER,
-        genre_id INTEGER,
-        game_id INTEGER,
-        genre_id INTEGER,
+    CREATE TABLE IF NOT EXISTS genres (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    PRIMARY KEY (id),
+    CONSTRAINT unique_genre_name UNIQUE(name));
 
     CREATE TABLE IF NOT EXISTS games (
-        id INTEGER GENERATED ALWAYS AS IDENTITY,
-        title VARCHAR(255) UNIQUE NOT NULL,
-        description TEXT UNIQUE NOT NULL,
-        release_date DATE NOT NULL,
-        publisher_id INTEGER,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES publishers(id) ON DELETE SET NULL,
-        );
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    release_date DATE NOT NULL,
+    publisher_id INTEGER,
+    PRIMARY KEY (id),
+    CONSTRAINT unique_game_title UNIQUE(title),
+    CONSTRAINT fk_publisher_id FOREIGN KEY (publisher_id) REFERENCES publishers(id) ON DELETE SET NULL);
+
+    CREATE TABLE IF NOT EXISTS covers (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER,
+    url TEXT NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT unique_cover_url UNIQUE (url),
+    CONSTRAINT unique_game_id UNIQUE (game_id),
+    CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE );
+
+
+    CREATE TABLE IF NOT EXISTS games_genres (
+    id INTEGER GENERATED ALWAYS AS IDENTITY,
+    game_id INTEGER,
+    genre_id INTEGER,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE SET NULL);
+
 `;
 
 async function main() {
--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     connectionString:
         process.env.NODE_ENV === "development"
             ? process.env.DB_DEVELOPMENT_CONNECTION_STRING
-            : undefined,
+            : process.env.DATABASE_PUBLIC_URL,
 });
 
 module.exports = pool;
