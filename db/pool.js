ff788910e3e1e98b9975b9faeb8b94b5fc04f2fa
Ignacio Gandur <gandurjuanignacio@gmail.com>
2024-09-29 02:06:24 -0300

    Modified connection string variable

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     connectionString:
         process.env.NODE_ENV === "development"
             ? process.env.DB_DEVELOPMENT_CONNECTION_STRING
-            : process.env.DATABASE_PUBLIC_URL,
+            : process.env.DB_PRODUCTION_CONNECTION_STRING,
 });
 
 module.exports = pool;
